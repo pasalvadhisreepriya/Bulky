4bbb2d822938593def4cc39dc015067b20b6f27a
pasalvadhisreepriya <pasalvadhi.sreepriya@kellton.com>
2023-05-19 20:55:06 +0530

    Add project files.

---
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,119 @@
+﻿
+
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BulkyBook.DataAccess.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id=1, Name="Action", DisplayOrder=1 },
+                 new Category { Id=2, Name="SciFi", DisplayOrder=2 },
+                  new Category { Id=3, Name="History", DisplayOrder=3 }
+                );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Title = "Phoebe Fortunes",
+                    Author = "Abby Muscles",
+                    Description = "Pzexcrtvybunrctvybuni",
+                    ISBN = "SWD999900111",
+                    ListPrice = 95,
+                    Price = 86,
+                    Price50 = 78,
+                    Price100 = 80,
+                    CategoryId = 1,
+                    ImageUrl=""
+                },
+                new Product
+                {
+                    Id = 2,
+                    Title = "Dark Skies",
+                    Author = "Valerie Stimac",
+                    Description = "zexrctvybunjmbyumio ",
+                    ISBN = "CAW777777701",
+                    ListPrice = 80,
+                    Price = 20,
+                    Price50 = 30,
+                    Price100 = 65,
+                    CategoryId = 1,
+                    ImageUrl=""
+                },
+                new Product
+                {
+                    Id = 3,
+                    Title = "Vanish by Dawn",
+                    Author = "JD Wells",
+                    Description = "zxctvybunjcgvbhjnvn",
+                    ISBN = "RITO5555501",
+                    ListPrice = 80,
+                    Price = 40,
+                    Price50 = 38,
+                    Price100 = 70,
+                    CategoryId = 1,
+                    ImageUrl=""
+                },
+                new Product
+                {
+                    Id = 4,
+                    Title = "Cotton Candy",
+                    Author = "Ted Kooser",
+                    Description = "zxrctvybunimokoyujhtrfgdsdcfvgbhjnkm ",
+                    ISBN = "WS3333333301",
+                    ListPrice = 70,
+                    Price = 63,
+                    Price50 = 50,
+                    Price100 = 60,
+                    CategoryId = 2,
+                    ImageUrl=""
+                },
+                new Product
+                {
+                    Id = 5,
+                    Title = "The Rock",
+                    Author = "LJ Ross",
+                    Description = "mbvcxzsedrftyuiop;lkjhgfdsasxcvbn ",
+                    ISBN = "SOTJ1111111101",
+                    ListPrice = 75,
+                    Price = 24,
+                    Price50 = 35,
+                    Price100 = 60,
+                    CategoryId = 2,
+                    ImageUrl=""
+                },
+                new Product
+                {
+                    Id = 6,
+                    Title = "The Last Book of Wonder",
+                    Author = "Lord Dunsany",
+                    Description = "qwertyuioplkjhgfdsaZxcvbnmngfd ",
+                    ISBN = "FOT000000001",
+                    ListPrice = 85,
+                    Price = 23,
+                    Price50 = 25,
+                    Price100 = 70,
+                    CategoryId = 3,
+                    ImageUrl=""
+                }
+                );
+        }
+    }
+}
+
+
---
+++ Bulky.DataAccess/Migrations/20230519152145_Initial.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BulkyBook.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "PostalCode",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "StreetAddress",
+                table: "AspNetUsers");
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Action" },
+                    { 2, 2, "SciFi" },
+                    { 3, 3, "History" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Author", "CategoryId", "Description", "ISBN", "ImageUrl", "ListPrice", "Price", "Price100", "Price50", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Abby Muscles", 1, "Pzexcrtvybunrctvybuni", "SWD999900111", "", 95.0, 86.0, 80.0, 78.0, "Phoebe Fortunes" },
+                    { 2, "Valerie Stimac", 1, "zexrctvybunjmbyumio ", "CAW777777701", "", 80.0, 20.0, 65.0, 30.0, "Dark Skies" },
+                    { 3, "JD Wells", 1, "zxctvybunjcgvbhjnvn", "RITO5555501", "", 80.0, 40.0, 70.0, 38.0, "Vanish by Dawn" },
+                    { 4, "Ted Kooser", 2, "zxrctvybunimokoyujhtrfgdsdcfvgbhjnkm ", "WS3333333301", "", 70.0, 63.0, 60.0, 50.0, "Cotton Candy" },
+                    { 5, "LJ Ross", 2, "mbvcxzsedrftyuiop;lkjhgfdsasxcvbn ", "SOTJ1111111101", "", 75.0, 24.0, 60.0, 35.0, "The Rock" },
+                    { 6, "Lord Dunsany", 3, "qwertyuioplkjhgfdsaZxcvbnmngfd ", "FOT000000001", "", 85.0, 23.0, 70.0, 25.0, "The Last Book of Wonder" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                type: "nvarchar(21)",
+                maxLength: 21,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostalCode",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "State",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StreetAddress",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
